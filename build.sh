83e94bd1d955a0cbbe65f7c916a8435a4ed859f9
tmeralus <tmeralus@gmail.com>
2018-11-27 09:51:04 -0500

    add docker build script

---
+++ build.sh
@@ -0,0 +1,7 @@
+sudo docker build -t ansible ~/github/docker-playground/Docker/ansible/
+sudo docker build -t ansible-srv01 ~/github/docker-playground/Docker/ansible-srv01/
+sudo docker build -t ansible-srv02 ~/github/docker-playground/Docker/ansible-srv02/
+sudo docker build -t ansible-srv03 ~/github/docker-playground/Docker/ansible-srv03/
+sudo docker build -t ansible-srv04 ~/github/docker-playground/Docker/ansible-srv04/
+sudo docker build -t redis-master ~/github/docker-playground/Docker/redis-master/
+sudo docker build -t redis-slave ~/github/docker-playground/Docker/redis-slave/
