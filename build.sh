2b5843af9a3cdaa7b6132280484003e51c73c4dd
tmeralus <tmeralus@gmail.com>
2018-11-27 14:31:12 -0500

    updated docker playground. added build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,18 @@
-sudo docker build -t ansible ~/github/docker-playground/Docker/ansible/
-sudo docker build -t ansible-srv01 ~/github/docker-playground/Docker/ansible-srv01/
-sudo docker build -t ansible-srv02 ~/github/docker-playground/Docker/ansible-srv02/
-sudo docker build -t ansible-srv03 ~/github/docker-playground/Docker/ansible-srv03/
-sudo docker build -t ansible-srv04 ~/github/docker-playground/Docker/ansible-srv04/
-sudo docker build -t redis-master ~/github/docker-playground/Docker/redis-master/
-sudo docker build -t redis-slave ~/github/docker-playground/Docker/redis-slave/
+sudo docker build -t ansible-master ~/github/docker-playground/docker/ansible/
+sudo docker build -t ansible-srv01 ~/github/docker-playground/docker/ansible-node1/
+sudo docker build -t ansible-srv01 ~/github/docker-playground/docker/ansible-node2/
+sudo docker build -t ansible-srv01 ~/github/docker-playground/docker/ansible-node3/
+sudo docker build -t ansible-srv01 ~/github/docker-playground/docker/ansible-node4/
+sudo docker build -t ansible-master ~/github/docker-playground/docker/redis-master/
+sudo docker build -t ansible-master ~/github/docker-playground/docker/redis-slave/
+
+#docker run -d -it -p 22 -t ansible --bind 10.1.2.10
+#docker run -d -it -p 22 -t ansible/node1 --bind 10.1.2.11
+#docker run -d -it -p 22 -t ansible/node2 --bind 10.1.2.12
+#docker run -d -it -p 22-t ansible/node3 --bind 10.1.2.13
+#docker run -d -it -p 22-t ansible/node4 --bind 10.1.2.14
+#docker run -d -it -p 6379 -t redis-master --bind 10.1.2.201
+#docker run -d -it -p 26379 -t redis-slave --bind 10.1.2.202
+
+#sudo docker build -t mysql ~/github/twitterstream/docker/mysql/
+#sudo docker build -t redis ~/github/twitterstream/docker/redis/
